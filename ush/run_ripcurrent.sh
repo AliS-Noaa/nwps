4b456f5e041665e4666f084e941327a8d19a1d00
AliS-Noaa <ali.salimi@noaa.gov>
2025-08-15 14:44:08 +0000

    correcting cycle format for rip and runup file names

--- ush/run_ripcurrent.sh
+++ ush/run_ripcurrent.sh
@@ -349,7 +349,7 @@ done
 #CONT
 
 #FORT23="${WFO}_${NET}_${contour}m_${CGnumber}_ripprob.${fullname}"
-FORT23="${NET}.t${cycle}z.${contour}m_${CGnumber}_ripprob.${WFO}.txt"
+FORT23="${NET}.t${cycle%??}z.${contour}m_${CGnumber}_ripprob.${WFO}.txt"
 cp ${RIPDATA}/fort.23 ${RIPDATA}/${FORT23}
 
 #exit 0
